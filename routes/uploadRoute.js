7007ea04b7028ee82a99916d7485332e0365e314
gitmvfs <marcossantos@gmail.com>
2024-03-01 06:46:29 -0300

    doc: trocado os comentários do código

--- routes/uploadRoute.js
+++ routes/uploadRoute.js
@@ -1,29 +1,29 @@
 const router = require("express").Router()
 const uploadCSV = require("../utils/multerStorage")
 const csvToJson = require("convert-csv-to-json")
-const {resolve} = require("path")
 
 router.post('/upload-csv', uploadCSV.single('csvFile'), async (req, res) => {
     if (!req.file) {
-        return res.status(400).json({ message: 'No file uploaded' });
+        return res.status(400).json({ message: 'Falha ao salvar arquivo' });
     }
 
     const jsonResponse = csvToJson.getJsonFromCsv(req.file.path)
+    
     console.log(jsonResponse)
     try {
         res.status(200)
             .json({
                 status: "200",
-                msg: "File upload success",
+                msg: "Arquivo salvo com sucesso",
                 dataFile: jsonResponse
             });
 
     } catch (error) {
-        console.error('Error processing CSV:', error);
+        console.error('Erro ao processar arquivo: ', error);
         res.status(500)
             .json({
                 status:"500", 
-                msg: 'Internal server error' 
+                msg: 'Erro do servidor' 
             });
     }
 });
